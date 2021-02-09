28f8f12890174d42c8cfae3b370f05b9be91e6df
Andy <hempel.andy@gmail.com>
2021-02-09 17:55:15 +0100

    add support for NUCLEO_F042K6

---
+++ variants/NUCLEO_F042K6‎/variant.cpp
@@ -0,0 +1,78 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2021, STMicroelectronics
+ * All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ *******************************************************************************
+ */
+
+#include "pins_arduino.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Digital PinName array
+const PinName digitalPin[] = {
+  PA_10, //D0
+  PA_9,  //D1
+  PA_12, //D2 - no PWM
+  PB_0,  //D3
+  PB_7,  //D4
+  PB_6,  //D5
+  PB_1,  //D6
+  PF_0,  //D7 - no PWM
+  PF_1,  //D8 - no PWM
+  PA_8,  //D9
+  PA_11, //D10
+  PB_5,  //D11
+  PB_4,  //D12
+  PB_3,  //D13 - LED
+  PA_0,  //D14//A0
+  PA_1,  //D15//A1
+  PA_3,  //D16//A2
+  PA_4,  //D17//A3
+  PA_5,  //D18//A4
+  PA_6,  //D19//A5
+  PA_7,  //D20//A6
+  PA_2,  //D21 - STLink Tx (no A7)
+  PA_15  //D22 - STLink Rx
+};
+
+/**
+  * @brief  System Clock Configuration
+  * @param  None
+  * @retval None
+  */
+WEAK void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1);
+}
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
